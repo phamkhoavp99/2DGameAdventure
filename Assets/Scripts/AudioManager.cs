3b4007beb72820b5dc47aafc766b44eb5653b8d5
phamkhoavp99 <58515339+phamkhoavp99@users.noreply.github.com>
2021-04-16 22:44:12 +0700

    Second commit

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,43 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-[System.Serializable]
 
-public class Sound
-{
-    public string name;
-    public AudioClip clip;
-
-    [Range(0f, 1f)]
-    public float volume = 0.7f;
-    [Range(0.5f, 1.5f)]
-    public float pitch = 1f;
-
-    [Range(0f, 0.5f)]
-    public float randomVolume = 0.1f;
-    [Range(0f, 0.5f)]
-    public float randomPitch = 0.1f;
-    private AudioSource source;
-    public void SetSource (AudioSource _source)
-    {
-        source = _source;
-        source.clip = clip;
-    }
-    public void Play()
-    {
-        source.volume = volume + (1 + Random.Range(-randomVolume / 2f, randomVolume / 2f));
-
-        source.pitch = pitch + (1 + Random.Range(-randomPitch / 2f, randomPitch / 2f));
-        source.Play();
-    }
-
-}
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField]
-    Sound[] sounds;
-
-    void Start()
-    {
-        
-    }
 
 }
