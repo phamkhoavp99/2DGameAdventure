9231c2509b6428ab7722f4d103e41b7c2ad7db47
phamkhoavp <52957433+phamkhoavp@users.noreply.github.com>
2021-04-27 01:01:14 +0700

    update 2st

---
+++ Assets/Scripts/SoundOnOff.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundOnOff : MonoBehaviour
+{
+    [SerializeField]
+    Image soundOnIcon;
+    [SerializeField]
+    Image soundOffIcon;
+    private bool muted = false;
+    public void OnButoonPress()
+    {
+        if (muted == false)
+        {
+            muted = true;
+            AudioListener.pause = true;
+        }
+
+        else
+        {
+            muted = false;
+            AudioListener.pause = false;
+        }
+        Save();
+        UpdateButtonIcon();
+    }
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("muted"))
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+        UpdateButtonIcon();
+        AudioListener.pause = muted;
+    }
+    private void UpdateButtonIcon()
+    {
+        if (muted == false)
+        {
+            soundOnIcon.enabled = true;
+            soundOffIcon.enabled = false;
+        }
+        else
+        {
+            soundOnIcon.enabled = false;
+            soundOffIcon.enabled = true;
+        }
+    }
+    private void Load()
+    {
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+    }
+}
